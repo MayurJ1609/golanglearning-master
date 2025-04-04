e6551c998fcb3d54b56a000f807de90cc524348e
Mayur J <mayur.jaganmohan@gmail.com>
2025-04-04 20:04:51 +0530

    changes for udemy final project

---
+++ 05udemy-final-project/cmd/web/handlers.go
@@ -0,0 +1,49 @@
+package main
+
+import "net/http"
+
+func (app *Config) HomePage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "home.page.gohtml", nil)
+}
+
+func (app *Config) LoginPage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "login.page.gohtml", nil)
+}
+
+func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
+	_ = app.Session.RenewToken(r.Context())
+
+	// parse form post
+	err := r.ParseForm()
+	if err != nil {
+		app.ErrorLog.Println(err)
+	}
+
+	// get email and password from form post
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	user, err := app.Models.User.GetByEmail(email)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Invalid credentials")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	// check pass
+	// validPassword, err =
+
+}
+
+func (app *Config) Logout(w http.ResponseWriter, r *http.Request) {
+}
+
+func (app *Config) RegisterPage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "register.page.gohtml", nil)
+}
+
+func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
+}
+
+func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
+}
---
+++ 05udemy-final-project/cmd/web/main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"database/sql"
+	"final-project/data"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPort = "80"
+
+func main() {
+	// connect to database
+	db := initDB()
+	db.Ping()
+
+	// create loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// create sessions
+	session := initSession()
+
+	// create channels
+
+	// create waitgroup
+	wg := sync.WaitGroup{}
+
+	// set up the application config
+	app := Config{
+		Session:  session,
+		DB:       db,
+		InfoLog:  infoLog,
+		ErrorLog: errorLog,
+		Wait:     &wg,
+		Models:   data.New(db),
+	}
+
+	// set up mail
+
+	// listen for signals
+	go app.listenForShutdown()
+
+	// listen for web connections
+	app.serve()
+}
+
+func (app *Config) serve() {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	app.InfoLog.Println(fmt.Sprintf("Starting web server on port %s", webPort))
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func initDB() *sql.DB {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Can't connect to Postgres")
+	}
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0
+	dsn := os.Getenv("DSN")
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres is not yet ready...")
+		} else {
+			log.Println("Connected to Postgres!")
+			return connection
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+		log.Println("Backing off for 1 seconds...")
+		time.Sleep(1 * time.Second)
+		counts++
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func initSession() *scs.SessionManager {
+	// set up session
+	session := scs.New()
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+	return session
+}
+
+func initRedis() *redis.Pool {
+	redisPool := &redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+	return redisPool
+}
+
+func (app *Config) listenForShutdown() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	app.shutdown()
+	os.Exit(0)
+}
+
+func (app *Config) shutdown() {
+	// perform any cleanup tasks
+	app.InfoLog.Println("would run cleanup tasks...")
+
+	// bock until wg is empty
+	app.Wait.Wait()
+
+	app.InfoLog.Println("Closing channels and shutting down application...")
+}
